43181f263c9a00562be2d4cbc575915e9a5736c0
Rajat sharma <rajatsharma369007@gmail.com>
2018-03-03 11:48:38 +0530

    Update push_button.c

--- Led_on_off_Push_button/push_button.c
+++ Led_on_off_Push_button/push_button.c
@@ -1,11 +1,10 @@
 void main()
 {
-     p1=0xFF;
+     p1=0xFF;            // initializing all the push buttons (input ports) as 1
 
      while(1)
      {
-       p2=~p1;
+       p2=~p1;           // when p1 is low (button is pushed) then p2 will be high
        delay_ms(500);
      }
-
 }
